976bb921572fcff31365727f0cb2009de5dd8a3d
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-09-04 19:41:51 -0300

    Update components.js

--- app/src/theme/components.js
+++ app/src/theme/components.js
@@ -7,55 +7,55 @@ const components = ()=> {
         MuiPaper: {
             styleOverrides:{
                 root: {
-                boxShadow: '0 0 0.3em #aaa',
+                    boxShadow: '0 0 0.3em #aaa',
                 }
             }
         },
         MuiTextField: {
             defaultProps: {
-            size: 'small',
+                size: 'small',
             }
         },
         MuiOutlinedInput: {
             styleOverrides: {
-            input: {
-                fontSize: 12,
-            },
+                input: {
+                    fontSize: 12,
+                },
             }
         },
         MuiInputLabel: {
             styleOverrides:{
-            root:{
-                fontSize: 12,
-            },
-            shrink: {
-                fontSize: 16
-            }
+                root:{
+                    fontSize: 12,
+                },
+                shrink: {
+                    fontSize: 16
+                }
             }
         },
         MuiFormControlLabel: {
             styleOverrides:{
-            label:{
-                fontSize: 12,
-            }
+                label:{
+                    fontSize: 12,
+                }
             }
         },
         MuiFormHelperText: {
             styleOverrides: {
-            root: {
-                fontSize: 10
-            }
+                root: {
+                    fontSize: 10
+                }
             }
         },
         MuiCheckbox: {
             defaultProps: {
-            size: 'small'
+                size: 'small'
             }
         },
         MuiButton: {
             defaultProps: {
-            size: 'small',
-            variant: 'contained'
+                size: 'small',
+                variant: 'contained'
             }
         }
     })
