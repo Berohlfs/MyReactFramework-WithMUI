0c4791aba156733684f592cd56fd215ae5568f3b
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-09-19 16:11:31 -0300

    UPDATE axios.js
    Make client toasts mirror the errors sent by the server

--- app/src/config/axios.js
+++ app/src/config/axios.js
@@ -9,43 +9,33 @@ const APIInstance = (navigate)=> {
 
     const instance = axios.create({
         baseURL: 'https://hp-api.onrender.com',
-        timeout: 20000,
+        timeout: 10000,
         // withCredentials: true
     })
 
     instance.interceptors.response.use(
         response=> response,
         error=> {
+            if(error?.code === 'ECONNABORTED' && error?.message.includes('timeout')) {
 
-            if(error?.response?.status === 401){ // Unauthorized
+                toast.error('Servidor indisponível.', {toastId: 'timeout'})
 
-                navigate && navigate('/')
-                Cookies.remove('access')
-                toast.warning('Acesso inválido ou expirado (401).', {toastId: 'invalid-token'})
+            }else if(error?.response?.status === 500){
 
-            }else if(error?.response?.status === 400){ // Bad Request
+                toast.warning('Erro interno do servidor (500).', {toastId: 'internal-server-error'})
 
-                toast.warning('Dados inválidos (400).', {toastId: 'invalid-client'})
+            }else if(error?.response?.status){
 
-            }else if(error?.response?.status === 404){ // Not Found
+                if(error?.response?.status === 401){
+                    navigate && navigate('/')
+                    Cookies.remove('access')
+                }
 
-                toast.warning('Instância não existente (404).', {toastId: 'not-found'})
+                toast.warning(`${error.response?.data?.message} (${error.response.status})`, {toastId: 'server-feedback'})
 
-            }else if(error?.response?.status === 409){ // Conflict
+            }else{
 
-                toast.warning('Instância já existente (409).', {toastId: 'instance-override'})
-
-            }else if(error?.response?.status === 500){ // Internal Server Error
-
-                toast.error('Erro interno do servidor (500).', {toastId: 'server-error'})
-
-            }else if(error.code === 'ECONNABORTED' && error.message.includes('timeout')) { // Request Timeout
-
-                toast.error('Não foi possível se conectar ao servidor.', {toastId: 'connection-error'})
-
-            }else{ // Unknown
-
-                toast.error('Houve um erro.', {toastId: 'generic-error'})
+                toast.error('Houve um erro.', {toastId: 'unmapped-error'})
 
             }
 
